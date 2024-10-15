75556e9efd6294eb20311b816de7ecde76f4db44
inhakJeon <ij3512@naver.com>
2024-10-15 13:29:56 +0900

    feat: add DB Testcode

--- ChatServer/main.cpp
+++ ChatServer/main.cpp
@@ -1,10 +1,46 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QTableView>
+#include <QSqlQueryModel>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlTableModel>
 
+static bool createConnection()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("data.db");
+    if (!db.open( )) return false;
+
+    QSqlQuery query;
+    query.exec("CREATE TABLE IF NOT EXISTS user(idx INTEGER Primary Key, "
+                     "id VARCHAR(20) NOT NULL, pw VARCHAR(20) NOT NULL, permission VARCHAR(20));");
+    query.exec("INSERT INTO user VALUES(101, 'Yongsu', 'Kang', '1');");
+    query.exec("INSERT INTO user(id, pw, permission) VALUES('Soomi', 'Kim', '1')");
+    query.exec("INSERT INTO user(id, pw, permission) VALUES "
+                     "('Hanmi', 'Lee', '1'), ('YoungJin', 'Suh', '1'), ('YoungHwa', 'Ryu', '1');");
+
+    return true;
+}
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    createConnection();
+    QSqlTableModel queryModel;
+    queryModel.setTable("user");
+    queryModel.select( );
+
+    queryModel.setHeaderData(0, Qt::Horizontal, QObject::tr("IDX"));
+    queryModel.setHeaderData(1, Qt::Horizontal, QObject::tr("ID"));
+    queryModel.setHeaderData(2, Qt::Horizontal, QObject::tr("PW"));
+    queryModel.setHeaderData(3, Qt::Horizontal, QObject::tr("Permission"));
+
+    QTableView *tableview = new QTableView;
+    tableview->setModel(&queryModel);
+    tableview->setWindowTitle(QObject::tr("Query Model"));
+    tableview->show( );
     MainWindow w;
     w.show();
     return a.exec();
